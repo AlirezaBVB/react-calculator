3e368be3bdf90d5232ddff84bc134c7c47ffca49
AlirezaBVB <Alirezaxz5p@gmail.com>
2024-08-15 15:09:38 +0330

    Design & Button Functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [val, setVal] = useState('')
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="calculator">
+          <form action="">
+            <div className='display'>
+              <input type="text" value={val} />
+            </div>
+            <div>
+              <input type="button" value="AC" onClick={e => setVal('')} />
+              <input type="button" value="DE" onClick={e => setVal(val.slice(0, -1))} />
+              <input type="button" value="." onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="/" onClick={e => setVal(val + e.target.value)} />
+            </div>
+            <div>
+              <input type="button" value="7" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="8" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="9" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="*" onClick={e => setVal(val + e.target.value)} />
+            </div>
+            <div>
+              <input type="button" value="4" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="5" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="6" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="+" onClick={e => setVal(val + e.target.value)} />
+            </div>
+            <div>
+              <input type="button" value="1" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="2" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="3" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="-" onClick={e => setVal(val + e.target.value)} />
+            </div>
+            <div>
+              <input type="button" value="00" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="0" onClick={e => setVal(val + e.target.value)} />
+              <input type="button" value="=" className='equal' />
+            </div>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
